658edbce07ee9fa332e8ef2430f06674342f2fea
RajatSinghPinnacle <Rajat.Singh@pinnacleway.work>
2024-06-03 15:47:06 +0530

    changes in User Profile

--- src/app/Components/user-profile/user-profile.component.ts
+++ src/app/Components/user-profile/user-profile.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+// import  userProfile from './json-data/user_profile.json';
+import { UserProfileService } from '../../services/user-profile.service';
 
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
-export class UserProfileComponent {
-  constructor(private router: Router) {}
+export class UserProfileComponent implements OnInit {
+  userProfile: any;
+
+  constructor(private router: Router , private http : HttpClient, private userProfileService : UserProfileService) {}
+
+  ngOnInit(){
+    this.userProfileService.getUserProfile().subscribe(data => {
+      this.userProfile = data;
+      console.log(this.userProfile); // You can do further processing here
+    });
+  }
+
   navigateToEditProfile() {
     this.router.navigate(['userprofile/edit']);
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,7 +32,8 @@ import { SettingComponent } from './components/user-profile/setting/setting.comp
     AppRoutingModule,
     MaterialModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [
     provideAnimationsAsync()
